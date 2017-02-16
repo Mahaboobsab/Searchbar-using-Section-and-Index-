e2524ac78b16c58cb130c851fd25d04f70079a92
Meheboob <mahaboobsab.n@neorays.com>
2017-02-16 17:10:32 +0530

    Initial Commit

---
+++ SearchText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchText
+//
+//  Created by Mahaboobsab Nadaf on 16/02/17.
+//  Copyright © 2017 Meheboob Nadaf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SearchText/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SearchText
+//
+//  Created by Mahaboobsab Nadaf on 16/02/17.
+//  Copyright © 2017 Meheboob Nadaf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
